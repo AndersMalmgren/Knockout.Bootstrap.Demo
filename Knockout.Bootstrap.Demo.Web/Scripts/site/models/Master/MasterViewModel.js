76c59db5ba4df98ae7285316fbf98667e4990fe6
AndersMalmgren <anders.malmgren@agero.se>
2013-09-17 14:05:08 +0200

    Added MVC4 bundle support

--- Knockout.Bootstrap.Demo.Web/Scripts/site/models/Master/MasterViewModel.js
+++ Knockout.Bootstrap.Demo.Web/Scripts/site/models/Master/MasterViewModel.js
@@ -1,33 +1,4 @@
-﻿Demo = {
-    define: function (definition) {
-        if (definition.prototype) {
-            definition.init.prototype = definition.prototype;
-            definition.init.prototype.constructor = definition.init;
-        }
-
-        if (definition.meta) {
-            definition.init.meta = definition.meta;
-        }
-
-        return definition.init;
-    },
-    modelLoader: function (model, callback) {
-        var onloaded = function (data) {
-            var instance = new model(data);
-
-            if (callback)
-                callback(instance);
-        };
-
-        if (model.meta && model.meta.url) {
-            $.getJSON(model.meta.url, null, onloaded);
-        } else {
-            onloaded();
-        }
-    }
-};
-
-Demo.MasterViewModel = Demo.define({
+﻿Demo.MasterViewModel = Demo.define({
     init: function (version) {
         this.view = ko.observable();
         this.version = version;
---
+++ Knockout.Bootstrap.Demo.Web/Scripts/site/utils.js
@@ -0,0 +1,29 @@
+﻿(function(Demo) {
+    Demo.define = function(definition) {
+        if (definition.prototype) {
+            definition.init.prototype = definition.prototype;
+            definition.init.prototype.constructor = definition.init;
+        }
+
+        if (definition.meta) {
+            definition.init.meta = definition.meta;
+        }
+
+        return definition.init;
+    };
+
+    Demo.modelLoader = function(model, callback) {
+        var onloaded = function(data) {
+            var instance = new model(data);
+
+            if (callback)
+                callback(instance);
+        };
+
+        if (model.meta && model.meta.url) {
+            $.getJSON(model.meta.url, null, onloaded);
+        } else {
+            onloaded();
+        }
+    };
+})(window.Demo = window.Demo = {});
